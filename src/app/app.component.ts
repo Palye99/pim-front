91529542c4e2e5d10bbdce3bdd69bb9d58590aff
Théo <theo.mouton@axopen.com>
2021-08-24 19:32:56 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Container, Main} from 'tsparticles';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,93 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pim-front';
+
+  id = 'tsparticles';
+  particlesOptions = {
+    background: {
+      color: {
+        value: '#ffffff'
+      }
+    },
+    fpsLimit: 60,
+    interactivity: {
+      detectsOn: 'canvas',
+      events: {
+        onClick: {
+          enable: true,
+          mode: 'push'
+        },
+        onHover: {
+          enable: false
+        },
+        resize: true
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40
+        },
+        push: {
+          quantity: 4
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4
+        }
+      }
+    },
+    particles: {
+      color: {
+        value: '#000000'
+      },
+      links: {
+        color: '#000000',
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1
+      },
+      collisions: {
+        enable: true
+      },
+      move: {
+        direction: 'none',
+        enable: true,
+        outMode: 'bounce',
+        random: false,
+        speed: 1,
+        straight: false
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800
+        },
+        value: 80
+      },
+      opacity: {
+        value: 0.5
+      },
+      shape: {
+        type: 'circle'
+      },
+      size: {
+        random: true,
+        value: 5
+      }
+    },
+    detectRetina: true
+  };
+
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
+
+  particlesInit(main: Main): void {
+    console.log(main);
+
+    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {FormsModule} from '@angular/forms';
 import {LoginComponent} from './components/login/login.component';
 import { PasswordComponent } from './components/password/password.component';
 import { EmailComponent } from './components/email/email.component';
+import {NgParticlesModule} from 'ng-particles';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { EmailComponent } from './components/email/email.component';
     AngularFireAuthModule,
     AngularFirestoreModule,
     FormsModule,
+    NgParticlesModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
